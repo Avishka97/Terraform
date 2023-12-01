22f6697b50a3e1504c273bb8c87a4af137fa332e
Avishka97 <48411264+Avishka97@users.noreply.github.com>
2023-12-01 12:48:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "main" {
   name     = var.resourcegroup
   location = var.location
