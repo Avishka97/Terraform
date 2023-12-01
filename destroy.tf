df1db8c533f3aa3978d1c5fd5083f51f17186821
Avishka97 <48411264+Avishka97@users.noreply.github.com>
2023-12-01 13:24:35 +0530

    Update destroy.tf

--- destroy.tf
+++ destroy.tf
@@ -1,3 +1,60 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name = "${var.prefix}-resourcegroup"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.prefix}-pip"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic1"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_network_interface" "internal" {
+  name                = "${var.prefix}-nic2"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_network_security_group" "webserver" {
+  name                = "tls_webserver"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.internal.id
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Destruction of all resources
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
 resource "null_resource" "destroy_resources" {
   triggers = {
     destroy = timestamp()
