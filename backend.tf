71a7ab265f27597e97d8503ccf2f69a4faba48e8
Avishka97 <48411264+Avishka97@users.noreply.github.com>
2023-12-01 12:38:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {  
   backend "azurerm" {
   resource_group_name  = "TerraformRG"
-  storage_account_name = "terraformstgavi"
+  storage_account_name = "terraformstgavi1"
   container_name       = "terraformcontainer"
   key                  = "terraform.tfstate"
   }
