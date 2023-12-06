0ec4aef653a54ee138ff6a4c3c8d0abf4b523098
Avishka97 <48411264+Avishka97@users.noreply.github.com>
2023-12-06 12:53:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_network_interface" "main" {
   location            = azurerm_resource_group.main.location
 
   ip_configuration {
-    name                          = ${var.vm1_name}-nic1-public
+    name                          = "${var.vm1_name}-nic1-public"
     subnet_id                     = azurerm_subnet.Terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
@@ -43,7 +43,7 @@ resource "azurerm_network_interface" "internal" {
   location            = azurerm_resource_group.main.location
 
   ip_configuration {
-    name                          = ${var.vm1_name}-nic2-internal
+    name                          = "${var.vm1_name}-nic2-internal"
     subnet_id                     = azurerm_subnet.Terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
