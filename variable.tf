407f031ee43bd8e91eeef220af63ed553aee2b2f
Avishka97 <48411264+Avishka97@users.noreply.github.com>
2023-12-06 12:30:29 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -15,3 +15,34 @@ variable "location" {
   type = string
   description = "The Azure Region in which all resources in this example should be created."
 }
+
+variable "resource_group_name" {
+  type        = string
+  description = "Declare the resource group for this environment"
+}
+
+variable "vnet" {
+  type = object({
+    address_space = list(string)
+  })
+}
+
+variable "subnet" {
+  type = map(object({
+    subnet_name      = string
+    address_prefixes = list(string)
+  }))
+  description = "Provide an object with the subnet type name and ip range"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Administrator user name for virtual machine"
+  sensitive   = false
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password must meet Azure complexity requirements"
+  sensitive   = true
+}
