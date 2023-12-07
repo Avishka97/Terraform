238fd445d3471640a329202cad234019e6942e51
Avishka97 <48411264+Avishka97@users.noreply.github.com>
2023-12-07 12:56:09 +0530

    Update codecoverage.ps1

--- codecoverage.ps1
+++ codecoverage.ps1
@@ -1,4 +1,5 @@
 $config = New-PesterConfiguration
 $config.Run.Path = "$(System.DefaultWorkingDirectory)/TERRAFORM.TESTS.PS1"
 $config.CodeCoverage.Enabled = $true
+$config.Output.Verbosity = "Detailed"
 Invoke-Pester -Configuration $config
